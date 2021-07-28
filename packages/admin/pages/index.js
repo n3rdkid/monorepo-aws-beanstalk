277ccdabd1e59ad6452e4004f1cd6e6c77f0ed04
Saurav <sauravads123@gmail.com>
2021-07-28 14:03:36 +0545

    admin test : zip without node_modules

--- packages/admin/pages/index.js
+++ packages/admin/pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to Admin App!!!
+          Welcome to Admin App YAYAY
         </h1>
        </main>
     </div>
