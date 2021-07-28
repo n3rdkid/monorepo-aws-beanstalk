7ce632438e3094dad123d22c9b922e74402a1f98
Saurav <sauravads123@gmail.com>
2021-07-28 17:15:42 +0545

    chaning common

---
+++ test.sh
@@ -0,0 +1,14 @@
+number_of_tags=$(git tag | wc -l)
+if [ $number_of_tags -lt 2 ]; then
+    echo "Creating production build for first time"
+    exit 0
+fi
+old_tag=$(git describe --abbrev=0 --tags $(git rev-list --tags --skip=1  --max-count=1)) 
+echo $old_tag
+new_tag=$(git describe --abbrev=0 --tags $(git rev-list --tags --max-count=1))
+echo $new_tag
+diff_files=$(git diff $old_tag $new_tag --name-only | grep -c packages/consumer packages/common || true)    
+echo $diff_files
+if [[ ${diff_files} -ne 0 ]]; then
+     echo there is diff
+fi
