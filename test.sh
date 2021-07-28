53861b14a1637ddc67dd4bda2df0f7d45baf123a
Saurav <sauravads123@gmail.com>
2021-07-28 17:22:38 +0545

    minor : upated workflow

--- test.sh
+++ test.sh
@@ -7,8 +7,9 @@ old_tag=$(git describe --abbrev=0 --tags $(git rev-list --tags --skip=1  --max-c
 echo $old_tag
 new_tag=$(git describe --abbrev=0 --tags $(git rev-list --tags --max-count=1))
 echo $new_tag
-diff_files=$(git diff $old_tag $new_tag --name-only | grep -c packages/consumer packages/common || true)    
+diff_files_consumer=$(git diff $old_tag $new_tag --name-only | grep -c packages/consumer || true)    
+diff_files_common=$(git diff $old_tag $new_tag --name-only | grep -c packages/common || true)  
 echo $diff_files
-if [[ ${diff_files} -ne 0 ]]; then
+if [[ ${diff_files} -ne 0 ] ||[ ${diff_files_common} -ne 0 ] ]; then
      echo there is diff
 fi
